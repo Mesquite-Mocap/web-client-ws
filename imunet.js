a85c773a29fbdd6f35fa9aaef3a8ae0c8aec95b0
sivareddy <sivahrez@gmail.com>
2023-03-14 13:47:42 -0700

    imunet data storage

--- imunet.js
+++ imunet.js
@@ -2,19 +2,20 @@ const WebSocket = require('ws');
 const fs = require('fs');
 
 const ws = new WebSocket.Server({ host: '0.0.0.0', port: 8555 });
-
+var i = 0;
 // Open a writable stream to the local file
 
 // When a new websocket connection is established
 ws.on('connection', function connection(ws) {
     console.log('WebSocket connection established');
     //create a new folder for every new connection
-    var d = new Date();
-    var n = d.getTime();
-    var dir = './' + n;
+
+    var dir = './' + "siva_sm31_"+i;
+    
     if (!fs.existsSync(dir)){
         fs.mkdirSync(dir);
     }
+    i++;
     var stream1 = fs.createWriteStream(dir + '/acce.csv');
     var stream2 = fs.createWriteStream(dir + '/gyro.csv');
     var stream3 = fs.createWriteStream(dir + '/magnet.csv');
@@ -24,6 +25,8 @@ ws.on('connection', function connection(ws) {
         // console.log('Received message:', data);
         data = data.toString();
         data = data.split(',');
+        var d = new Date();
+        data[0] = d.getTime();
         console.log(data);
         stream2.write(data[0] + ',' + data[1] + ',' + data[2] + ',' + data[3] +'\n');
         stream1.write(data[0] + ',' + data[4] + ',' + data[5] + ',' + data[6] +'\n');
