040f4b70803e3dc1ea572386307c60ab0072fca1
sivareddy <sivahrez@gmail.com>
2023-03-14 10:11:57 -0700

    added a new server for teh imunet data recieving

---
+++ imunet.js
@@ -0,0 +1,20 @@
+const WebSocket = require('ws');
+const fs = require('fs');
+
+const ws = new WebSocket.Server({ port: 8555 });
+
+// Open a writable stream to the local file
+const stream = fs.createWriteStream('data.txt');
+
+// When a new websocket connection is established
+ws.on('connection', function connection(ws) {
+    console.log('WebSocket connection established');
+
+    // When a message is received on the websocket connection
+    ws.on('message', function incoming(data) {
+        console.log('Received message:', data);
+
+        // Write the received data to the local file
+        stream.write(data + '\n');
+    });
+});
